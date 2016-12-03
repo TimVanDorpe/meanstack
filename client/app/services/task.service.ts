7c83ccc374b8739373e8550a41efa57335f939d0
TimVanDorpe <tim.van.dorpe@hotmail.com>
2016-12-03 22:13:44 +0100

    update and delete

--- client/app/services/task.service.ts
+++ client/app/services/task.service.ts
@@ -4,16 +4,32 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class TaskService{
-
-    constructor(private http:Http)
-    {
-        console.log('Task Service Initialized...')
-
+    constructor(private http:Http){
+        console.log('Task Service Initialized...');
     }
-
+    
     getTasks(){
-        return this.http.get('http://localhost:3000/api/tasks')
-        .map(res => res.json());
-
+        return this.http.get('/api/tasks')
+            .map(res => res.json());
+    }
+    
+    addTask(newTask){
+        console.log("add new task" + newTask);
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.post('/api/task', JSON.stringify(newTask), {headers: headers})
+            .map(res => res.json());
+    }
+    
+    deleteTask(id){
+        return this.http.delete('/api/task/'+id)
+            .map(res => res.json());
+    }
+    
+    updateStatus(task){
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.put('/api/task/'+task._id, JSON.stringify(task), {headers: headers})
+            .map(res => res.json());
     }
 }
